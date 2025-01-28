b087f6c4b0d1022b1fef22be72b90e4290fc2c1f
Rahul-067 <bt23eee067@students.vnit.ac.in>
2025-01-28 18:08:50 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,30 @@
+//Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, Bigint
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 23547487787545
+
+
+ // Reference (Non primitive)
+
+ //Array, Objects, Function
+
+ const heros = ["shatimaan", "naagraj", "doga"];
+ let myObj = {
+    name: "Rahul",
+    age: 21,
+
+ }
+ console.log(typeof scoreValue);
